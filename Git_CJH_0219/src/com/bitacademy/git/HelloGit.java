fdeed97b991b7a9f513d63e22ff9e6d646c185d3
clllc1985 <clllc1985@naver.com>
2016-09-02 13:22:37 +0900

    파일 되돌리기

--- Git_CJH_0219/src/com/bitacademy/git/HelloGit.java
+++ Git_CJH_0219/src/com/bitacademy/git/HelloGit.java
@@ -6,11 +6,6 @@ public class HelloGit {
 		// TODO Auto-generated method stub
 		System.out.println("안녕 Git");
 		System.out.println("점심 뭐먹지 배고프다");
-		
-		//1차 수정 - 그냥 저장만
-		//일단 커밋 말고 저장만
-		
-		//이번 주석 삽입 후 저장 + 커밋
 	}
 
 }
